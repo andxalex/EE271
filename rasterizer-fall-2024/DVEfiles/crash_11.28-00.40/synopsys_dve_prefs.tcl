e68dd62c3b62ce49c2a8d41240aba87ee116c87f
Andreas Alexandrou <andalex@caddy04.stanford.edu>
2024-12-03 10:36:10 -0800

    added assignment 3

--- rasterizer-fall-2024/DVEfiles/crash_11.28-00.40/synopsys_dve_prefs.tcl
+++
@@ -1,5 +0,0 @@
-# DVE version M-2017.03_Full64
-# Preferences written on Sun Nov 17 00:56:34 2024
-gui_set_var -name {read_pref_file} -value {true}
-gui_create_pref_key -category {Globals} -key {load_detail_for_funcov} -value_type {bool} -value {false}
-gui_set_var -name {read_pref_file} -value {false}
